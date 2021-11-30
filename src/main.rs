1063f8816e5df0348bbb73a10f691272032a1b26
Ashe Erickson <aerickson@extraspace.com>
2021-11-30 16:42:30 -0700

    Base rust read-from-file structure

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    assert!(args.len() == 2, "Expected exactly 1 argument");
+    println!("Reading {}", args[1]);
+
+    let path = args[1].as_str();
+    let file_handle = File::open(path).expect("Unable to open file");
+    let read_stream = BufReader::new(file_handle);
+
+    let lines = read_stream.lines();
+
+    for (index, line) in lines.enumerate() {
+        println!("{: >4} > {}", index, line.expect("Read error"));
+    }
+}
