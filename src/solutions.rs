156adeec823d34bc4daf1d7841b43b68742c2e5e
Ashe <ashe@tempest.dev>
2021-12-01 23:07:44 -0700

    Part 2

--- src/solutions.rs
+++ src/solutions.rs
@@ -47,19 +47,46 @@ pub fn part_one<'a>(lines : impl Iterator<Item = &'a str>) -> i32 {
     return total_lateral * total_vertical;
 }
 
-pub fn part_two<'a>(lines : impl Iterator<Item = &'a str>) -> u32 {
-    panic!("Not implemented yet");
-    let measurements : Vec<u32> = lines.map(|line|
-        line.parse().unwrap()
-    ).collect();
+pub fn part_two<'a>(lines : impl Iterator<Item = &'a str>) -> i32 {
+    let steps = lines.map(|line| {
+        let words : Vec<&str> = line.split(' ').collect();
 
-    let mut num_increasing = 0;
+        assert!(words.len() == 2, "Line did not have two words");
 
-    for index in 0..(measurements.len() - 3) {
-        if measurements[index] < measurements[index + 3] {
-            num_increasing += 1;
+        let direction = match words[0] {
+            "up" => Direction::Up,
+            "down" => Direction::Down,
+            "forward" => Direction::Forward,
+            "backward" => Direction::Backward,
+            _ => panic!("Unknown direction")
+        };
+
+        let length = words[1].parse().unwrap();
+
+        NavStep {
+            direction,
+            length
+        }
+    });
+
+    let mut total_lateral : i32 = 0;
+    let mut total_vertical : i32 = 0;
+    let mut aim : i32 = 0;
+    
+    for step in steps {
+        match step.direction {
+            Direction::Forward => {
+                total_lateral += step.length;
+                total_vertical += aim * step.length
+            },
+            Direction::Backward => {
+                total_lateral -= step.length;
+                total_vertical -= aim * step.length
+            }
+            Direction::Down => aim += step.length,
+            Direction::Up => aim -= step.length,
         }
     }
 
-    return num_increasing
+    return total_lateral * total_vertical;
 }
