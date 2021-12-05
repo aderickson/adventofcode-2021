df9554e21bb834e62f4d963ad29ebed149f993ac
Ashe <ashe@tempest.dev>
2021-12-05 01:01:59 -0700

    Day 5

--- src/solutions.rs
+++ src/solutions.rs
@@ -1,151 +1,142 @@
+use std::{collections::HashMap, str::FromStr};
+
 use crate::file::FileReader;
 
-static WIN_PATTERNS : [u32 ; 10] = [
-    0b1111100000000000000000000,
-    0b0000011111000000000000000,
-    0b0000000000111110000000000,
-    0b0000000000000001111100000,
-    0b0000000000000000000011111,
-    0b1000010000100001000010000,
-    0b0100001000010000100001000,
-    0b0010000100001000010000100,
-    0b0001000010000100001000010,
-    0b0000100001000010000100001
-];
-
-struct BingoBoard {
-    numbers : [u32 ; 25],
-    marks : u32
+#[derive(PartialEq, Eq, Hash, Copy, Clone)]
+struct Point {
+    x: u32,
+    y: u32
 }
 
-impl BingoBoard {
-    pub fn parse(reader : &mut FileReader) -> Self {
-        let marks = 0;
-        let mut numbers : Vec<u32> = Vec::new();
+impl FromStr for Point {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let coords : Vec<&str> = s.split(',').collect();
+
+        assert!(coords.len() == 2, "Wrong number of coordinates");
+
+        let point = Point {
+            x: coords[0].parse().unwrap(),
+            y: coords[1].parse().unwrap()
+        };
+
+        return Ok(point)
+    }
+}
+
+struct Line {
+    start: Point,
+    end: Point,
+
+    last: Option<Point>
+}
+
+impl Line {
+    pub fn new(start : Point, end : Point) -> Self {
+        return Line {
+            start,
+            end,
+            last: None
+        };
+    }
+
+    fn get_step(goal : u32, current : u32) -> i32 {
+        if goal == current {
+            0
+        } else if goal > current {
+            1
+        } else {
+            -1
+        }
+    }
+}
+
+impl Iterator for Line {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.last.is_none() {
+            self.last = Some(self.start);
+            return self.last
+        }
+
+        if self.last.unwrap() == self.end {
+            return None
+        }
+
+        let last = self.last.unwrap();
+
+        let step_x = Line::get_step(self.end.x, last.x);
+        let step_y = Line::get_step(self.end.y, last.y);
+
+        self.last = Some(Point {
+            x: (last.x as i32 + step_x) as u32,
+            y: (last.y as i32 + step_y) as u32
+        });
+
+        return self.last
+    }
+}
+
+pub fn part_one(lines : FileReader) -> u32 {
+    let lines = lines.map(|line| {
+        let points : Vec<&str> = line.as_str().split(" -> ").collect();
         
-        for _ in 0..5 {
-            let row = reader.next().unwrap();
+        let start = points[0].parse().unwrap();
+        let end = points[1].parse().unwrap();
 
-            for num in row.split_whitespace() {
-                numbers.push(num.parse().unwrap())
+        return Line::new(start, end);
+    }).filter(|line| 
+        !(line.start.x != line.end.x && line.start.y != line.end.y)
+    );
+
+    let mut intersections = HashMap::new();
+
+    for line in lines {
+        for point in line {
+            if let Some(current) = intersections.get_mut(&point) {
+                let value = *current + 1;
+                *current = value;
+            } else {
+                intersections.insert(point, 1);
             }
-        }
-
-        return BingoBoard {
-            marks,
-            numbers: numbers.try_into().unwrap()
         }
     }
 
-    pub fn mark_board(&mut self, number : u32) {
-        let mut index = 0;
-        let mut found = false;
+    let count = intersections.iter().filter(|(_, value)| {
+        **value >= 2
+    }).count();
 
-        for i in 0..self.numbers.len() {
-            if self.numbers[i] == number {
-                index = i;
-                found = true;
-                break;
-            }
-        }
-
-        if !found {
-            return;
-        }
-
-        let marker = 2u32.pow(index as u32);
-        self.marks |= marker;
-    }
-
-    pub fn has_won(&self) -> bool{
-        for win_pattern in WIN_PATTERNS {
-            if self.marks & win_pattern == win_pattern {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    pub fn get_score(&self) -> u32 {
-        let mut score = 0;
-
-        for index in 0..self.numbers.len() {
-            let bit_flag = 2u32.pow(index as u32);
-            if !self.marks & bit_flag != 0 {
-                score += self.numbers[index];
-            }
-        }
-
-        return score;
-    }
+    return count as u32;
 }
 
-pub fn part_one(mut lines : FileReader) -> u32 {
-    let first_line = lines.next().unwrap();
+pub fn part_two(lines : FileReader) -> u32 {
+    let lines = lines.map(|line| {
+        let points : Vec<&str> = line.as_str().split(" -> ").collect();
+        
+        let start = points[0].parse().unwrap();
+        let end = points[1].parse().unwrap();
 
-    let numbers = first_line.split(',').map(|s| s.parse::<u32>().unwrap());
-    let mut boards  = Vec::new();
+        return Line::new(start, end);
+    });
 
-    while let Some(_) = lines.next() {
-        boards.push(BingoBoard::parse(&mut lines));
-    }
+    let mut intersections = HashMap::new();
 
-    let mut winning_board = 0;
-    let mut found_board = false;
-    let mut last_num = 0;
-
-    'outer: for number in numbers {
-        last_num = number;
-        for (index, board) in boards.iter_mut().enumerate() {
-            board.mark_board(number);
-            if board.has_won() {
-                winning_board = index;
-                found_board = true;
-                break 'outer;
+    for line in lines {
+        for point in line {
+            if let Some(current) = intersections.get_mut(&point) {
+                let value = *current + 1;
+                *current = value;
+            } else {
+                intersections.insert(point, 1);
             }
         }
     }
 
-    if !found_board {
-        panic!("No board won!");
-    }
+    let count = intersections.iter().filter(|(_, value)| {
+        **value >= 2
+    }).count();
 
-    return last_num * boards[winning_board].get_score();
-}
-
-pub fn part_two(mut lines : FileReader) -> u32 {
-    let first_line = lines.next().unwrap();
-
-    let numbers = first_line.split(',').map(|s| s.parse::<u32>().unwrap());
-    let mut boards  = Vec::new();
-
-    while let Some(_) = lines.next() {
-        boards.push(BingoBoard::parse(&mut lines));
-    }
-
-    let num_boards = boards.len();
-    let mut last_num = 0;
-    let mut winning_boards = Vec::new();
-    let mut last_board = 0;
-
-    'outer: for number in numbers {
-        last_num = number;
-        for (index, board) in boards.iter_mut().enumerate() {
-            if winning_boards.contains(&index) {
-                continue;
-            }
-
-            board.mark_board(number);
-            if board.has_won() {
-                winning_boards.push(index);
-                if winning_boards.len() == num_boards {
-                    last_board = index;
-                    break 'outer;
-                }
-            }
-        }
-    }
-
-    return last_num * boards[last_board].get_score();
+    return count as u32;
 }
